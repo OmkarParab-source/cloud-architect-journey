bc67ea9a313a47b014010c22476e066718f16cd5
OmkarP <omkarparab21@gmail.com>
2026-03-24 13:10:06 +0530

    feat: complete vpc networking layer with routing and subnet associations

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -46,3 +46,39 @@ resource "aws_internet_gateway" "this" {
     Name = "${var.vpc_name}-igw"
   })
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-public-rt"
+  })
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-private-rt"
+  })
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = aws_subnet.private
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
