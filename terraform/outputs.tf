2a7588d0402d880a1a5c9320d9fc06b61b4512fc
OmkarP <omkarparab21@gmail.com>
2026-04-02 01:08:41 +0530

    refactor: remove unnecessary root outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,28 +2,8 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "public_subnet_ids" {
-  value = module.networking.public_subnet_ids
-}
-
 output "private_subnet_ids" {
   value = module.networking.private_subnet_ids
-}
-
-output "public_subnets" {
-  value = module.networking.public_subnets
-}
-
-output "private_subnets" {
-  value = module.networking.private_subnets
-}
-
-output "alb_sg_id" {
-  value = module.security.alb_sg_id
-}
-
-output "web_sg_id" {
-  value = module.security.web_sg_id
 }
 
 output "alb_dns_name" {
