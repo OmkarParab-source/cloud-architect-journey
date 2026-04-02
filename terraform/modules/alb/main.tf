bed49fc1b0e18e3268df4479b74bbdf52339e6ff
OmkarP <omkarparab21@gmail.com>
2026-04-02 23:26:39 +0530

    infra: update health check interval for faster detection

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -24,7 +24,7 @@ resource "aws_lb_target_group" "web" {
     path                = "/"
     protocol            = "HTTP"
     matcher             = "200-399"
-    interval            = 30
+    interval            = 10
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
